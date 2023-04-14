7d96623eb3d72f97febbdeff4d4c1412d2dcedb4
MatteoMancuso-92 <Matteotrieste@outlook.com>
2023-04-14 14:54:06 +0200

    js

---
+++ script.js
@@ -0,0 +1,36 @@
+const numberKmElement = document.querySelector ('#number-km')
+
+const userAgeElement = document.querySelector ('#user-age')
+
+const generates = document.querySelector('#generates');
+
+// console.log ('prova')
+
+generates.addEventListener('click', function() {
+
+   // console.log ('prova2')
+const numberKm = parseInt(numberKmElement.value)
+
+const userAge = parseInt(userAgeElement.value)
+
+	
+    const price =(0.21);
+   console.log(price * numberKm);
+
+   const totalPrice =(numberKm * price)
+
+   if (userAge < 18) {
+    console.log (  totalPrice - totalPrice * 20 / 100);
+    document.getElementById ('price').innerHTML = ( ' ' + totalPrice - totalPrice * 20 / 100)
+   }
+   else if(userAge >= 65){
+    console.log (  totalPrice - totalPrice * 40 / 100);
+    document.getElementById ('price').innerHTML = ( ' ' + totalPrice - totalPrice * 40 / 100)
+   }
+ 
+   else {
+    console.log(totalPrice)
+    document.getElementById ('price').innerHTML = ( ' ' + totalPrice)
+   }
+
+});
